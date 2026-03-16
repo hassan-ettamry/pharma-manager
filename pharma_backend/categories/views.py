914c799010a3e06d0db2d76be724839a75231f80
hassan-ettamry <hassan1ettamry@gmail.com>
2026-03-16 11:58:26 +0000

    feat: add medicament viewset and stock alerts endpoint

--- pharma_backend/categories/views.py
+++ pharma_backend/categories/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db import models
 
-# Create your views here.
+from .models import Medicament
+from .serializers import MedicamentSerializer
+
+
+class MedicamentViewSet(ModelViewSet):
+
+    queryset = Medicament.objects.filter(est_actif=True)
+    serializer_class = MedicamentSerializer
+
+    @action(detail=False, methods=["get"])
+    def alertes(self, request):
+
+        low_stock = Medicament.objects.filter(
+            stock_actuel__lte=models.F("stock_minimum"),
+            est_actif=True
+        )
+
+        serializer = self.get_serializer(low_stock, many=True)
+
+        return Response(serializer.data)
