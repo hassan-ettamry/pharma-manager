2c7dd318d1e4971abe254138e4ef2e08642e6f8c
hassan-ettamry <hassan1ettamry@gmail.com>
2026-03-16 11:50:16 +0000

    feat: create sales models

--- pharma_backend/ventes/models.py
+++ pharma_backend/ventes/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from apps.medicaments.models import Medicament
 
-# Create your models here.
+
+class Vente(models.Model):
+
+    STATUT_CHOICES = [
+        ("EN_COURS", "En cours"),
+        ("COMPLETEE", "Complétée"),
+        ("ANNULEE", "Annulée"),
+    ]
+
+    reference = models.CharField(max_length=50, unique=True)
+
+    date_vente = models.DateTimeField(auto_now_add=True)
+
+    total_ttc = models.DecimalField(max_digits=10, decimal_places=2)
+
+    statut = models.CharField(
+        max_length=20,
+        choices=STATUT_CHOICES,
+        default="EN_COURS"
+    )
+
+    notes = models.TextField(blank=True)
+
+
+class LigneVente(models.Model):
+
+    vente = models.ForeignKey(
+        Vente,
+        on_delete=models.CASCADE,
+        related_name="lignes"
+    )
+
+    medicament = models.ForeignKey(
+        Medicament,
+        on_delete=models.PROTECT
+    )
+
+    quantite = models.IntegerField()
+
+    prix_unitaire = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    sous_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
