468a1146287902712e049ac09f0baef2808d7397
hassan-ettamry <hassan1ettamry@gmail.com>
2026-03-16 11:46:41 +0000

    feat: create category models

--- pharma_backend/categories/models.py
+++ pharma_backend/categories/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Catégorie de médicament
+    """
+    nom = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nom
