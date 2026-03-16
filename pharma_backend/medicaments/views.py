45a2a661f5214bf94e646a7344129f3fd093a8ec
hassan-ettamry <hassan1ettamry@gmail.com>
2026-03-16 13:17:55 +0000

    feat: implement medicament viewset with stock alerts endpoint

--- pharma_backend/medicaments/views.py
+++ pharma_backend/medicaments/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import F
 
-# Create your views here.
+from .models import Medicament
+from .serializers import MedicamentSerializer
+
+
+class MedicamentViewSet(ModelViewSet):
+
+    queryset = Medicament.objects.filter(est_actif=True)
+    serializer_class = MedicamentSerializer
+
+    def perform_destroy(self, instance):
+        """
+        Soft delete medicament
+        """
+        instance.est_actif = False
+        instance.save()
+
+    @action(detail=False, methods=["get"])
+    def alertes(self, request):
+        """
+        Return medicaments with low stock
+        """
+
+        low_stock = Medicament.objects.filter(
+            stock_actuel__lte=F("stock_minimum")
+        )
+
+        serializer = self.get_serializer(low_stock, many=True)
+
+        return Response(serializer.data)
