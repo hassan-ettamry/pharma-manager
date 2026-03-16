41ac61f56538b992cbdd1806163d8d2c892f7d54
hassan-ettamry <hassan1ettamry@gmail.com>
2026-03-16 11:48:09 +0000

    feat: add medicament model

--- pharma_backend/medicaments/models.py
+++ pharma_backend/medicaments/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from apps.categories.models import Category
 
-# Create your models here.
+
+class Medicament(models.Model):
+
+    nom = models.CharField(max_length=255)
+    dci = models.CharField(max_length=255)
+
+    categorie = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="medicaments"
+    )
+
+    forme = models.CharField(max_length=100)
+    dosage = models.CharField(max_length=100)
+
+    prix_achat = models.DecimalField(max_digits=10, decimal_places=2)
+    prix_vente = models.DecimalField(max_digits=10, decimal_places=2)
+
+    stock_actuel = models.IntegerField()
+    stock_minimum = models.IntegerField()
+
+    date_expiration = models.DateField()
+
+    ordonnance_requise = models.BooleanField(default=False)
+
+    date_creation = models.DateTimeField(auto_now_add=True)
+
+    est_actif = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nom
