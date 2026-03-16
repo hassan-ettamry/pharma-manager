77e7f511c634ec1e24e5521e1bb465ca6400136f
hassan-ettamry <hassan1ettamry@gmail.com>
2026-03-16 14:51:51 +0000

    docs: add swagger documentation for categories endpoints

--- pharma_backend/categories/views.py
+++ pharma_backend/categories/views.py
@@ -1,9 +1,32 @@
 from rest_framework.viewsets import ModelViewSet
+from drf_spectacular.utils import extend_schema_view, extend_schema
 
 from .models import Category
 from .serializers import CategorySerializer
 
 
+@extend_schema_view(
+    list=extend_schema(
+        description="Retrieve list of all medicament categories",
+        tags=["Categories"]
+    ),
+    retrieve=extend_schema(
+        description="Retrieve details of a specific category",
+        tags=["Categories"]
+    ),
+    create=extend_schema(
+        description="Create a new medicament category",
+        tags=["Categories"]
+    ),
+    update=extend_schema(
+        description="Update an existing category",
+        tags=["Categories"]
+    ),
+    destroy=extend_schema(
+        description="Delete a category",
+        tags=["Categories"]
+    ),
+)
 class CategoryViewSet(ModelViewSet):
 
     queryset = Category.objects.all()
