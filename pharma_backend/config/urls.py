bec465a8d64ffb04b48ae5c85829f483a9939571
hassan-ettamry <hassan1ettamry@gmail.com>
2026-03-16 12:45:48 +0000

    feat: connect ventes api to project urls

--- pharma_backend/config/urls.py
+++ pharma_backend/config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.urls import path, include
 
 urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema')),
+    path("api/v1/ventes/", include("ventes.urls")),
 ]
