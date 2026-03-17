c7f03278e672540952a31c7b55f924d2c882d876
hassan-ettamry <hassan1ettamry@gmail.com>
2026-03-17 10:20:13 +0000

    feat: configure axios with environment base URL

--- pharma_frontend/src/api/axiosConfig.js
+++ pharma_frontend/src/api/axiosConfig.js
@@ -4,7 +4,7 @@ import axios from "axios";
  * Axios instance for API calls
  */
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default axiosInstance;
