88d473c2dcf9a4c49654cb9098e3b7a4e3832535
hassan-ettamry <hassan1ettamry@gmail.com>
2026-03-17 13:34:58 +0000

    test: add basic unit test for medicament model

--- pharma_backend/medicaments/tests.py
+++ pharma_backend/medicaments/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Medicament
+from categories.models import Category
 
-# Create your tests here.
+
+class MedicamentTestCase(TestCase):
+
+    def setUp(self):
+        self.category = Category.objects.create(nom="Test")
+
+    def test_create_medicament(self):
+        medicament = Medicament.objects.create(
+            nom="TestMed",
+            dci="Test",
+            categorie=self.category,
+            forme="Comprime",
+            dosage="500mg",
+            prix_achat=5,
+            prix_vente=10,
+            stock_actuel=100,
+            stock_minimum=10,
+            date_expiration="2027-01-01"
+        )
+
+        self.assertEqual(medicament.nom, "TestMed")
