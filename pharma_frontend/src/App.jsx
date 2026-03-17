375e0f82031cd0090dd4594970e19fd2b54cf997
hassan-ettamry <hassan1ettamry@gmail.com>
2026-03-17 08:33:55 +0000

    feat: add dashboard and navigation routing

--- pharma_frontend/src/App.jsx
+++ pharma_frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage";
 import MedicamentsPage from "./pages/MedicamentsPage";
 import VentesPage from "./pages/VentesPage";
 
 function App() {
   return (
     <BrowserRouter>
+
+      {/* Navigation */}
+      <nav>
+        <Link to="/">Dashboard</Link> |{" "}
+        <Link to="/medicaments">Medicaments</Link> |{" "}
+        <Link to="/ventes">Ventes</Link>
+      </nav>
+
+      {/* Pages */}
       <Routes>
-        <Route path="/" element={<MedicamentsPage />} />
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/medicaments" element={<MedicamentsPage />} />
         <Route path="/ventes" element={<VentesPage />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
