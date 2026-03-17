0d2872701c96920ceae74ee2b57581129629d9e3
hassan-ettamry <hassan1ettamry@gmail.com>
2026-03-17 07:04:28 +0000

    feat: add ventes page placeholder

---
+++ pharma_frontend/src/pages/VentesPage.jsx
@@ -0,0 +1,11 @@
+/**
+ * Sales page (temporary)
+ */
+export default function VentesPage() {
+    return (
+      <div>
+        <h1>Ventes Page</h1>
+        <p>Sales module coming...</p>
+      </div>
+    );
+  }
