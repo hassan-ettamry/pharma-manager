429d029881959702baa1c7253f4bb8fb727d2b46
hassan-ettamry <hassan1ettamry@gmail.com>
2026-03-17 06:25:52 +0000

    feat: implement ventes api service with create and cancel functionality

---
+++ pharma_frontend/src/api/ventesApi.js
@@ -0,0 +1,29 @@
+import axiosInstance from "./axiosConfig";
+
+/**
+ * Fetch all sales
+ * @returns {Promise<Object>} List of ventes
+ */
+export const fetchVentes = async () => {
+  const response = await axiosInstance.get("/ventes/");
+  return response.data;
+};
+
+/**
+ * Create a new sale
+ * @param {Object} data - Sale payload
+ * @returns {Promise<Object>}
+ */
+export const createVente = async (data) => {
+  const response = await axiosInstance.post("/ventes/", data);
+  return response.data;
+};
+
+/**
+ * Cancel a sale
+ * @param {number} id - Vente ID
+ */
+export const cancelVente = async (id) => {
+  const response = await axiosInstance.post(`/ventes/${id}/annuler/`);
+  return response.data;
+};
